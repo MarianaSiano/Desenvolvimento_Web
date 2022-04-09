76b03b65ba46fbe619da5b119ed5d57023b06477
Mariana Siano <maryhsiano@hotmail.com>
2022-04-09 15:21:11 -0300

    Atualizando o arquivo escopo_variavel.js

--- JavaScript/js/escopo_variaveis.js
+++ JavaScript/js/escopo_variaveis.js
@@ -4,6 +4,17 @@ document.write('<h1>Escopo de Variáveis</h1>');
 //Escopo de variáveis
 var serie = 'Friends';
 
+//Escopo de Bloco
 if(true) {
     document.write(serie);
+    document.write("<br>");
 }
+
+document.write("<br>");
+
+//Função
+function teste() {
+    document.write(serie);
+}
+
+teste();
