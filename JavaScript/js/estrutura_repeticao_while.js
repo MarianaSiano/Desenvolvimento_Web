612543ff1f3f3a1ba892f271a5387e4239f795a2
Mariana Siano <maryhsiano@hotmail.com>
2022-04-11 13:33:03 -0300

    Adicionando os arquivos de 'Estutura de Repeticao -- While'

---
+++ JavaScript/js/estrutura_repeticao_while.js
@@ -0,0 +1,11 @@
+//Título da aula
+document.write('<h1>Estrutura de Repetição -- While</h1>');
+
+//While
+var x = 1;
+
+while(x <= 10) {
+    //Códigos
+    document.write(x + '<br>');
+    x += 3;
+}
