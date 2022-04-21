fc97032a2c7ae574e83dc0ac3d69320f0a3b98bc
Mariana Siano <maryhsiano@hotmail.com>
2022-04-21 19:34:11 -0300

    Adicionando o primeiro arquivo php

---
+++ PHP7/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Embutindo blocos PHP em Páginas HTML</title>
+    </head>
+
+    <body>
+        <?php
+            echo 'Tag padrão';
+        ?>
+
+        <br />
+
+        <?=
+            'Tag impressão';
+        ?>
+    </body>
+
+</html>
