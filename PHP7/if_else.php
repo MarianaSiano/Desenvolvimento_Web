fb6de078b5635db3b8d8c56d8ed39489ec6dde62
Mariana Siano <maryhsiano@hotmail.com>
2022-04-24 16:18:02 -0300

    Adicionando o arquivo php 'if/else'

---
+++ PHP7/if_else.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head lang="pt-br">
+        <meta charset="utf-8" />
+        <title>if/else em PHP</title>
+    </head>
+
+    <body>
+
+        <?php
+            if($condicao) {
+                //Trecho de código que será executado
+            }
+
+            else {
+                //Trecho de código que será executado
+            }
+        ?>
+
+        <!-- Ou -->
+        <?php
+            if($condicao) {
+                //Trecho de código que será executado
+            }
+
+            else if($condicao){
+                //Trecho de código que será executado
+            }
+
+            else {
+                //Trecho de código que será executado
+            }
+        ?>
+
+    </body>
+
+</html>
