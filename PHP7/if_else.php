b8ae4cc308d767ed557b44096660e798555a5e0c
Mariana Siano <maryhsiano@hotmail.com>
2022-04-26 13:39:09 -0300

    Testando Operadores Lógicos

--- PHP7/if_else.php
+++ PHP7/if_else.php
@@ -31,7 +31,17 @@
 
             //Operadores Lógicos 
             
-            
+            $valor1 = 25;
+            $valor2 = 23;
+            $valor3 = 10;
+
+            if($valor1 > $valor2 && $valor3 > $valor1) {
+                echo 'Deu verdadeiro, entrou no if! $valor1 é maior que o $valor2 e o $valor3 é maior que o $valor 1';
+            }
+
+            else {
+                echo 'Deu falso, entrou o else! Uma das condições passadas é falsa';
+            }
         ?>
 
         <!-- Ou -->
