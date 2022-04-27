2e25f1b1e6ec6cffedc01b093f6cc7a7d61caa49
Mariana Siano <maryhsiano@hotmail.com>
2022-04-27 19:08:29 -0300

    Adicionando arquivo php 'operador_ternario'

---
+++ PHP7/operador_ternario.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head lang="pt-br">
+        <meta charset="utf-8" />
+        <title>Operador Ternário em PHP</title>
+    </head>
+
+    <body> 
+    </body>
+
+</html>
