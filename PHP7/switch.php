829ff5758e5483428801be704ef0140c1581bebb
Mariana Siano <maryhsiano@hotmail.com>
2022-05-04 17:25:12 -0300

    Adicionando o arquivo php 'Switch'

---
+++ PHP7/switch.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head lang="pt-br">
+        <meta charset="utf-8" />
+        <title>Switch em PHP</title>
+    </head>
+
+    <body>
+        
+        <?php
+        ?>
+
+    </body>
+
+</html>
