f95e7a8a13f43f6d57669dd86b7d0a9acd6004d5
Mariana Siano <maryhsiano@hotmail.com>
2022-05-26 19:26:01 -0300

    Atualizando o Readme e adicionando arquivos PHP

---
+++ PHP7/comentarios.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head lang="pt-br">
+        <meta charset="utf-8" />
+        <title>Comentários em PHP</title>
+    </head>
+
+    <body>
+
+        <?php
+            //Comentário de uma linha 
+        ?>
+
+        <?php
+            #Comentário de uma linha
+        ?>
+
+        <?php
+           /* Comemtário 
+            de varias
+            linhas */ 
+        ?>
+        
+    </body>
+
+</html>
---
+++ PHP7/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head lang="pt-br">
+        <meta charset="utf-8" />
+        <title>Embutindo blocos PHP em Páginas HTML</title>
+    </head>
+
+    <body>
+
+        <?php
+            echo 'Tag padrão';
+        ?>
+
+        <br />
+
+        <?=
+            'Tag impressão';
+        ?>
+        
+    </body>
+
+</html>
